ab6fb0d3670b679eb5da7f2d04a67aa92f09808f
pelednoam <peled.noam@gmail.com>
2015-10-22 11:41:55 -0400

    plot a cigar

---
+++ src/misc.py
@@ -0,0 +1,79 @@
+import glob
+import os
+import shutil
+import numpy as np
+import csv
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+SUBJECTS_DIR = '/homes/5/npeled/space3/subjects'
+COPY_FROM = '/homes/5/npeled/space3/Downloads/for_noam'
+
+
+def copy_some_files():
+    neccesary_files = {'mri': ['aseg.mgz'], 'surf': ['rh.pial', 'lh.pial', 'rh.sphere.reg', 'lh.sphere.reg', 'lh.white', 'rh.white']}
+    files = glob.glob(os.path.join(COPY_FROM, '*.*'))
+    for full_file_name in files:
+        file = os.path.split(full_file_name)[1]
+        subject, file_name = file.split('_')
+        subject = subject.lower()
+        sub_folder = 'mri' if file_name in neccesary_files['mri'] else 'surf'
+        mkdirs(os.path.join(SUBJECTS_DIR, subject, sub_folder))
+        local_file = os.path.join(SUBJECTS_DIR, subject, sub_folder, file_name)
+        if os.path.isfile(local_file):
+            print('{} already exist'.format(local_file))
+        else:
+            print('copying {} to {}'.format(full_file_name, local_file))
+            shutil.copyfile(full_file_name, local_file)
+
+
+def mkdirs(path):
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
+
+def electrodes_npz_to_csv(npz_file, csv_file):
+    d = np.load(npz_file)
+    names, pos = d['names'], d['pos']
+    with open(csv_file, 'w') as fp:
+        writer = csv.writer(fp, delimiter=',')
+        writer.writerow(['', 'R', 'A', 'S'])
+        for name, pos in zip(names, pos):
+            values = [name]
+            values.extend(pos)
+            writer.writerow(values)
+
+
+def check_cigar(elc_length, r):
+    pos = np.array([21.5,   3.,   9.])
+    next_pos = np.array([26.5,   3.,   9.])
+    dist = np.linalg.norm(next_pos-pos)
+    elc_ori = (next_pos-pos) / dist # norm(elc_ori)=1mm
+    elc_line = np.array([pos + elc_ori*t for t in np.linspace(-elc_length/2.0, elc_length/2.0, 100)])
+    N = 1000000
+    points = np.zeros((N, 3))
+    points[:, 0] = np.random.uniform(0.0, 40.0, (N,))
+    points[:, 1] = np.random.uniform(-20.0, 20.0, (N,))
+    points[:, 2] = np.random.uniform(-20.0, 20.0, (N,))
+    dists = np.min(cdist(elc_line, points), 0)
+    inside = dists <= r
+    outside = dists > r
+    print(sum(inside))
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(elc_line[:, 0], elc_line[:, 1], elc_line[:, 2], c='k')
+    # ax.scatter(points[outside, 0], points[outside, 1], points[outside, 2], c='blue', edgecolors='none', alpha=0.1)
+    ax.scatter(points[inside, 0], points[inside, 1], points[inside, 2], c='red', edgecolors='none', alpha=0.3)
+    plt.show()
+
+if __name__ == '__main__':
+    # copy_some_files()
+    # electrodes_npz_to_csv('/homes/5/npeled/space3/subjects/mg79/electrodes/electrodes_positions.npz',
+    #     '/homes/5/npeled/space3/subjects/mg79/electrodes/mg79_RAS.csv')
+    check_cigar(4, 3)
+    print('finish!')
