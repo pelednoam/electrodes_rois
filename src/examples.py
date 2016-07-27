390ae717362405cbd6c11b37dad138a34c0c13b5
pelednoam <peled.noam@gmail.com>
2016-07-27 09:53:28 -0400

    Add examples

---
+++ src/examples.py
@@ -0,0 +1,32 @@
+import argparse
+
+from src import find_rois
+from src import utils
+from src import args_utils as au
+
+# 'mg72,mg76,mg83,mg85,mg88'
+def use_sftp(subject):
+    argv = ['-s', subject, '--sftp', '1', '--sftp_username', 'npeled',
+            '--sftp_domain', 'door.nmr.mgh.harvard.edu',
+            '--remote_subject_dir_template', '/autofs/cluster/neuromind/npeled/subjects/{subject}',
+            '--elecs_dir', '/home/noam/subjects/RAScoordinatessnapped',
+            '--write_compact_bipolar', '1', '--overwrite', '0']
+    args = find_rois.get_args(argv)
+    find_rois.run_for_all_subjects(args)
+
+
+def darpa(subject):
+    argv = ['-s', subject,
+            '--remote_subject_dir_template', '/space/huygens/1/users/kara/{subject}_SurferOutput',
+            '--remote_subject_dir_func', 'upper']
+    args = find_rois.get_args(argv)
+    find_rois.run_for_all_subjects(args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Electrodes labeling')
+    parser.add_argument('-s', '--subject', help='subject name', required=True, type=au.str_arr_type)
+    parser.add_argument('-f', '--function', help='function name', required=True)
+    args = utils.Bag(au.parse_parser(parser))
+    for subject in args.subject:
+        locals()[args.function](subject)
